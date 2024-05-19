b44baa51c7a2088e9a8b3f5b21cfbb54b426d208
Wunpeemai Boonta <peemai.exp@gmail.com>
2024-05-19 17:54:15 +0000

    remove_non_use

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Make sure the script stops if any command fails
-set -e
-
-# Install required packages using python3
-python3 install_requirements.py
-
-# Run the main code using python3
-python3 main.py
